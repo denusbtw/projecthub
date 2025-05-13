f380c4ede2861cf3d6f13c51b664a5cfb0703569
denusbtw <denisrozum@gmail.com>
2025-05-13 23:07:27 +0300

    feat(comments): Add Comment model + migrations file

--- projecthub/comments/models.py
+++ projecthub/comments/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+import textwrap
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from projecthub.core.models.base import UUIDModel, TimestampedModel
+from projecthub.tasks.models import Task
+
+
+class Comment(UUIDModel, TimestampedModel):
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        help_text=_("Task that owns this comment."),
+    )
+    parent = models.ForeignKey(
+        "Comment",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="replies",
+        help_text=_("Parent comment."),
+    )
+    body = models.CharField(max_length=2000, help_text=_("Body of comment."))
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True,
+        related_name="created_comments",
+        help_text=_("Author of comment"),
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+        constraints = [
+            models.CheckConstraint(
+                condition=~models.Q(parent_id=models.F("id")),
+                name="prevent_self_parent",
+            )
+        ]
+
+    def __str__(self):
+        body = textwrap.shorten(self.body, 20)
+        return f"{self.created_by.username}: {body} in {self.task.name}"
