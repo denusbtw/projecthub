1f27b19256af498bb7dfe12530230491d715f694
denusbtw <denisrozum@gmail.com>
2025-05-13 22:38:38 +0300

    feat(users): create UserFactory and fixtures in conftest.py

---
+++ projecthub/users/tests/factories.py
@@ -0,0 +1,37 @@
+import factory
+from django.utils import timezone
+from faker import Faker
+
+from projecthub.users.models import User
+
+faker = Faker()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    date_joined = factory.LazyFunction(
+        lambda: timezone.make_aware(faker.date_time_this_year())
+    )
+
+    @factory.post_generation
+    def password(self, create, extracted, **kwargs):
+        password = (
+            extracted
+            if extracted
+            else factory.Faker(
+                "password",
+                length=42,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True,
+            ).evaluate(None, None, extra={"locale": None})
+        )
+        self.set_password(password)
+
+    class Meta:
+        model = User
+        skip_postgeneration_save = True
