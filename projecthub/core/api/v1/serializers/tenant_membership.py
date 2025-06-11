9dc9bf960710eb0382bff749c6092920b1e62d4a
denusbtw <denisrozum@gmail.com>
2025-06-11 18:48:07 +0300

    feat(core): add validation check in TenantMembershipCreateSerializer so owner can't be TenantMembership user + tests

--- projecthub/core/api/v1/serializers/tenant_membership.py
+++ projecthub/core/api/v1/serializers/tenant_membership.py
@@ -23,6 +23,12 @@ class TenantMembershipCreateSerializer(serializers.ModelSerializer):
         model = TenantMembership
         fields = ("user", "role")
 
+    def validate_user(self, user):
+        tenant_owner_id = self.context["tenant_owner_id"]
+        if user.pk == tenant_owner_id:
+            raise serializers.ValidationError("Tenant owner cannot be a member.")
+        return user
+
 
 class TenantMembershipUpdateSerializer(serializers.ModelSerializer):
     class Meta:
--- projecthub/core/tests/api/v1/serializers/test_tenant_membership.py
+++ projecthub/core/tests/api/v1/serializers/test_tenant_membership.py
@@ -1,15 +1,17 @@
 import pytest
+from rest_framework.exceptions import ValidationError
 
 from projecthub.core.api.v1.serializers import (
     TenantMembershipUpdateSerializer,
     TenantMembershipListSerializer,
     TenantMembershipDetailSerializer,
+    TenantMembershipCreateSerializer,
 )
 from projecthub.core.models import TenantMembership
 
 
 @pytest.mark.django_db
-class TestTenantListSerializer:
+class TestTenantMembershipListSerializer:
 
     def test_role_is_correct(self, tenant_membership_factory):
         membership = tenant_membership_factory(role=TenantMembership.Role.USER)
@@ -18,7 +20,7 @@ class TestTenantListSerializer:
 
 
 @pytest.mark.django_db
-class TestTenantDetailSerializer:
+class TestTenantMembershipDetailSerializer:
 
     def test_role_is_correct(self, tenant_membership_factory):
         membership = tenant_membership_factory(role=TenantMembership.Role.USER)
@@ -27,8 +29,28 @@ class TestTenantDetailSerializer:
 
 
 @pytest.mark.django_db
-class TestTenantUpdateSerializer:
+class TestTenantMembershipCreateSerializer:
+
+    def test_error_if_user_is_tenant_owner(self, tenant):
+        data = {"user": tenant.owner_id, "role": TenantMembership.Role.USER}
+        context = {"tenant_owner_id": tenant.owner_id}
+        serializer = TenantMembershipCreateSerializer(data=data, context=context)
+
+        with pytest.raises(ValidationError) as exc:
+            serializer.is_valid(raise_exception=True)
+
+        assert "Tenant owner cannot be a member." in str(exc.value)
+
+    def test_no_error_if_user_is_not_tenant_owner(self, tenant, user):
+        data = {"user": user.pk, "role": TenantMembership.Role.USER}
+        context = {"tenant_owner_id": tenant.owner_id}
+        serializer = TenantMembershipCreateSerializer(data=data, context=context)
+        assert serializer.is_valid(), serializer.errors
+
+
+@pytest.mark.django_db
+class TestTenantMembershipUpdateSerializer:
 
     def test_no_error_if_empty_data(self, tenant_membership):
         serializer = TenantMembershipUpdateSerializer(tenant_membership, data={})
-        assert serializer.is_valid(), serializer.errors
+        assert serializer.is_valid(), serializer
