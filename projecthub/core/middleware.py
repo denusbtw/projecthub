a864fa418a2e50710cbcbdc40d7919743bd9f097
denusbtw <denisrozum@gmail.com>
2025-06-11 18:11:27 +0300

    feat(core): refactor tenant retrieval and handle inactive tenants in TenantMiddleware + tests

--- projecthub/core/middleware.py
+++ projecthub/core/middleware.py
@@ -1,28 +1,46 @@
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
+
+
 class TenantMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
+        self.whitelist_paths = [
+            "/admin",
+            "/api/auth",
+        ]
 
     def __call__(self, request):
-        sub_domain = self.get_sub_domain_from_request(request)
-
-        if sub_domain is None:
+        if self.is_whitelisted_path(request.path):
             request.tenant = None
             return self.get_response(request)
 
-        from .models import Tenant
+        sub_domain = self.get_sub_domain_from_request(request)
+        if sub_domain is None:
+            raise PermissionDenied("Subdomain required.")
 
-        try:
-            tenant = Tenant.objects.get(sub_domain=sub_domain)
-            request.tenant = tenant
-        except Tenant.DoesNotExist:
-            request.tenant = None
+        tenant = self.get_tenant(sub_domain)
+        if tenant is None:
+            raise Http404("Tenant is not found.")
 
+        request.tenant = tenant
         return self.get_response(request)
 
     def get_sub_domain_from_request(self, request):
         domain = request.get_host().split(":")[0]
         parts = domain.split(".")
-        return self.normalize_sub_domain(parts[0]) if len(parts) > 1 else None
+        return parts[0] if len(parts) > 1 else None
 
-    def normalize_sub_domain(self, sub_domain):
-        return sub_domain.replace("-", "_")
+    def get_tenant(self, sub_domain):
+        from .models import Tenant
+
+        try:
+            tenant = Tenant.objects.get(sub_domain=sub_domain)
+            if tenant.is_inactive:
+                raise PermissionDenied("Tenant is inactive.")
+            return tenant
+        except Tenant.DoesNotExist:
+            return None
+
+    def is_whitelisted_path(self, path):
+        return any(path.startswith(p) for p in self.whitelist_paths)
--- projecthub/core/tests/test_middleware.py
+++ projecthub/core/tests/test_middleware.py
@@ -1,4 +1,6 @@
 import pytest
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
 
 from projecthub.core.middleware import TenantMiddleware
 
@@ -6,21 +8,47 @@ from projecthub.core.middleware import TenantMiddleware
 @pytest.mark.django_db
 class TestTenantMiddleware:
 
+    def test_no_error_if_subdomain_is_none_and_whitelisted_path(self, rf):
+        request = rf.get("/admin/")
+        middleware = TenantMiddleware(lambda req: None)
+        middleware(request)
+
+    def test_error_if_no_subdomain(self, rf):
+        request = rf.get("/")
+        middleware = TenantMiddleware(lambda req: None)
+
+        with pytest.raises(PermissionDenied) as exc:
+            middleware(request)
+
+        assert "Subdomain required" in str(exc.value)
+
+    def test_no_error_if_subdomain(self, rf, tenant_factory):
+        tenant_factory(sub_domain="subdomain")
+        request = rf.get("/", HTTP_HOST="subdomain.localhost")
+        middleware = TenantMiddleware(lambda req: None)
+        middleware(request)
+
+    def test_error_if_tenant_with_this_subdomain_does_not_exist(self, rf):
+        request = rf.get("/", HTTP_HOST="subdomain.localhost")
+        middleware = TenantMiddleware(lambda req: None)
+
+        with pytest.raises(Http404) as exc:
+            middleware(request)
+
+        assert "Tenant is not found" in str(exc.value)
+
+    def test_error_if_tenant_is_inactive(self, rf, tenant_factory):
+        tenant_factory(sub_domain="subdomain", is_active=False)
+        request = rf.get("/", HTTP_HOST="subdomain.localhost")
+        middleware = TenantMiddleware(lambda req: None)
+
+        with pytest.raises(PermissionDenied) as exc:
+            middleware(request)
+
+        assert "Tenant is inactive" in str(exc.value)
+
     def test_tenant_is_set_if_exists(self, tenant, http_host, rf):
         request = rf.get("/", HTTP_HOST=http_host)
-        middleware = TenantMiddleware(lambda req: None)
-        middleware(request)
-        assert request.tenant == tenant
-
-    def test_tenant_is_none_if_does_not_exist(self, rf):
-        request = rf.get("/", HTTP_HOST="doesnotexist.localhost")
-        middleware = TenantMiddleware(lambda req: None)
-        middleware(request)
-        assert request.tenant is None
-
-    def test_tenant_middleware_resolves_normalized_subdomain(self, tenant_factory, rf):
-        tenant = tenant_factory(sub_domain="this_is_tenant")
-        request = rf.get("/", HTTP_HOST="this-is-tenant.localhost")
         middleware = TenantMiddleware(lambda req: None)
         middleware(request)
         assert request.tenant == tenant
